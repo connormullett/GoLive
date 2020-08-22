1b874fb0caf6ba00b70c73832d4e2f1dfa3cffd9
connor <connormullett@gmail.com>
2020-08-22 13:11:44 -0400

    unneccesary usings and remove stray file

--- GoLive.WebAPI/Startup.cs
+++ GoLive.WebAPI/Startup.cs
@@ -9,13 +9,10 @@ using GoLive.Helpers;
 using GoLive.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GoLive
 {
